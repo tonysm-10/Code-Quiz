86de8bd04a442562539c93b1ed7e7f61f9161dbd
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-03-15 15:13:44 -0700

    made the startiing page with the rules for the game and the button to start playing

---
+++ script.js
@@ -0,0 +1,17 @@
+var button = document.getElementById('strtGame')
+console.log(button)
+var display = document.getElementById("display")
+var timerAmount = document.getElementsByClassName('timerAmount')
+
+
+
+function firstQes(){
+   
+}
+
+function displayNone(){
+display.style = "display: none;"
+
+}
+
+button.addEventListener('click', displayNone)
